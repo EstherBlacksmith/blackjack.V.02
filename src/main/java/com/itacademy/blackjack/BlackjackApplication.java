205a1589311c585c9262d29938be663821c5fc73
Esther <githubEsther@gmail.com>
2026-01-21 17:47:33 +0100

    feat(app): add Game class demonstration in main method

--- src/main/java/com/itacademy/blackjack/BlackjackApplication.java
+++ src/main/java/com/itacademy/blackjack/BlackjackApplication.java
@@ -1,6 +1,7 @@
 package com.itacademy.blackjack;
 
 import com.itacademy.blackjack.deck.model.Deck;
+import com.itacademy.blackjack.game.model.Game;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -13,6 +14,14 @@ public class BlackjackApplication {
 		Deck deck = new Deck();
 	//	System.out.println(deck.getCardsSnapshot());
 		System.out.println(deck.draw());
+
+		Game game = new Game();
+		game.dealInitialCards();
+		System.out.println(game.getPlayerHand());
+		System.out.println(game.getCrupierHand());
+		System.out.println(game.getCrupierScore());
+		System.out.println(game.getPlayerScore());
+
 	}
 
 }
