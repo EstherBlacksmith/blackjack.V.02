f4b5007a08d5a114422e40e01cda603231a5bcc5
Esther <githubesther@gmail.com>
2026-01-22 12:18:27 +0100

    refactor(game): update Crupier model

---
+++ src/main/java/com/itacademy/blackjack/game/model/Crupier.java
@@ -0,0 +1,47 @@
+package com.itacademy.blackjack.game.model;
+
+import com.itacademy.blackjack.deck.model.Card;
+import com.itacademy.blackjack.deck.model.ScoringService;
+
+/**
+ * Simple class representing the dealer (Crupier) in Blackjack.
+ */
+public class Crupier {
+    private final Hand hand;
+
+    public Crupier(ScoringService scoringService) {
+        this.hand = new Hand(scoringService);
+    }
+
+    public void receiveCard(Card card) {
+        hand.addCard(card);
+    }
+
+    public int getScore() {
+        return hand.getScore();
+    }
+
+    public int getCardCount() {
+        return hand.getCardCount();
+    }
+
+    public boolean mustHit() {
+        return hand.getScore() <= 16;
+    }
+
+    public boolean mustStand() {
+        return hand.getScore() >= 17;
+    }
+
+    public boolean isBusted() {
+        return hand.isBusted();
+    }
+
+    public boolean hasBlackjack() {
+        return hand.isBlackjack();
+    }
+
+    public Hand getHand() {
+        return hand;
+    }
+}
