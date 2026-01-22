008c5c51769f2f5188eb29a872e6b433d913dd85
Esther <githubesther@gmail.com>
2026-01-22 12:17:59 +0100

    feat(game): add NO_RESULTS_YET state to GameResult enum

--- src/main/java/com/itacademy/blackjack/game/model/GameResult.java
+++ src/main/java/com/itacademy/blackjack/game/model/GameResult.java
@@ -1,5 +1,5 @@
 package com.itacademy.blackjack.game.model;
 
 public enum GameResult {
-    PLAYER_WINS, PUSH, CRUPIER_WINS, BLACKJACK
+    PLAYER_WINS, PUSH, CRUPIER_WINS, NO_RESULTS_YET, BLACKJACK
 }
