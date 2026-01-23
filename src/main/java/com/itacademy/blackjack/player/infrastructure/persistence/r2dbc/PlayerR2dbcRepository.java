fb65b278f4a6b0f762e7280aa4d332855e4a3317
Esther <githubEsther@gmail.com>
2026-01-23 20:00:18 +0100

    feat(db): add Docker Compose configuration for MySQL and MongoDB databases

--- src/main/java/com/itacademy/blackjack/player/infrastructure/persistence/r2dbc/PlayerR2dbcRepository.java
+++ src/main/java/com/itacademy/blackjack/player/infrastructure/persistence/r2dbc/PlayerR2dbcRepository.java
@@ -1,11 +1,10 @@
-package com.itacademy.blackjack.player.domain.repository;
+package com.itacademy.blackjack.player.infrastructure.persistence.r2dbc;
 
 import com.itacademy.blackjack.player.domain.model.Player;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Mono;
 
-import java.util.UUID;
-
-public interface PlayerRepository extends ReactiveCrudRepository<Player, UUID> {
+public interface PlayerR2dbcRepository  extends ReactiveCrudRepository<Player, String> {
     Mono<Player> findByName(String name);
+    Mono<Player> findById(String id);
 }
