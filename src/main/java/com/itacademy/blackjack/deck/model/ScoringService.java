6f086c1f0dd7e07ce9da896931e42f0830bcc163
Esther <githubEsther@gmail.com>
2026-01-23 12:58:21 +0100

    refactor(game): add @Service annotation to ScoringService

--- src/main/java/com/itacademy/blackjack/deck/model/ScoringService.java
+++ src/main/java/com/itacademy/blackjack/deck/model/ScoringService.java
@@ -1,7 +1,10 @@
 package com.itacademy.blackjack.deck.model;
 
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 
+@Service
 public class ScoringService {
     public int calculateHandScore(List<Card> hand) {
         int score = hand.stream().mapToInt(Card::getNumericValue).sum();
